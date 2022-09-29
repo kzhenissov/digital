5e7d32bcedd5f6e3c44f2f43595aca62a8353123
Kuanyshkhan <64012843+kzhenissov@users.noreply.github.com>
2022-09-29 15:51:22 +0600

    correct some code

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,8 @@
-document.querySelector('.menu__btn').addEventListener('click', () => {
-    document.querySelector('.menu__list').classList.toggle('menu__active');
-});
+// document.querySelector('.menu__btn').addEventListener('click', () => {
+//     document.querySelector('.menu__list').classList.toggle('menu__active');
+// });
+document.querySelector('.application__choose-item').onclick = () => {
+    this.onclick.style.color = "backgorund: red";
+}
+// console.log('clicked')
+
