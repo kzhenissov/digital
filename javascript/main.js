fa8a201bf3ece52564913112546e1e989f6a2876
Kuanyshkhan <64012843+kzhenissov@users.noreply.github.com>
2022-10-01 18:17:24 +0600

    add click menu btn

--- javascript/main.js
+++ javascript/main.js
@@ -1,6 +1,6 @@
-// document.querySelector('.menu__btn').addEventListener('click', () => {
-//     document.querySelector('.menu__list').classList.toggle('menu__active');
-// });
+document.querySelector('.menu__btn').addEventListener('click', () => {
+    document.querySelector('.menu__list').classList.toggle('menu__active');
+});
 // document.querySelector('.application__choose-item').onclick = () => {
 //     this.onclick.style.color = "backgorund: red";
 // }
