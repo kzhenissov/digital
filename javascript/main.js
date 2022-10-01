a9167529c2efeadd1ba390588d4419ebda47d1a9
Kuanyshkhan <64012843+kzhenissov@users.noreply.github.com>
2022-10-01 17:31:19 +0600

    correct brif select btn

--- javascript/main.js
+++ javascript/main.js
@@ -1,8 +1,13 @@
 // document.querySelector('.menu__btn').addEventListener('click', () => {
 //     document.querySelector('.menu__list').classList.toggle('menu__active');
 // });
-document.querySelector('.application__choose-item').onclick = () => {
-    this.onclick.style.color = "backgorund: red";
-}
+// document.querySelector('.application__choose-item').onclick = () => {
+//     this.onclick.style.color = "backgorund: red";
+// }
 // console.log('clicked')
+
+document.querySelector('.application__choose-list').onclick = (event) => {
+    if (!event.target.classList.contains('application__choose-btn')) return;
+    event.target.classList.toggle('application__choose-select')
+}
 
