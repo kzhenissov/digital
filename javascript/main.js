2f4f2414c1589e620e9ad3ad73ce5996a7627373
Kuanyshkhan <64012843+kzhenissov@users.noreply.github.com>
2022-10-25 16:29:55 +0600

    correct

--- javascript/main.js
+++ javascript/main.js
@@ -1,4 +1,3 @@
-
 changeTheme = () => {
     if (myCheck.checked) {
         document.getElementById("header").classList.add("bg-dark");
@@ -14,15 +13,32 @@ changeTheme = () => {
         document.getElementById("services").classList.remove("bg-dark");
     }
 }
+
 document.querySelector('.menu__btn').addEventListener('click', () => {
     document.querySelector('.menu__list').classList.toggle('menu__active');
 });
 document.querySelector('.application__choose-item').onclick = () => {
     this.onclick.style.color = "backgorund: red";
 }
-console.log('clicked')
+
 
 document.querySelector('.application__choose-list').addEventListener('click', (event) => {
     if (!event.target.classList.contains('application__choose-btn')) return;
     event.target.classList.toggle('application__choose-select')
 });
+
+
+// var myCheck = document.getElementById('myCheck');
+
+// if(myCheck.checked) {
+//     document.getElementById("header").classList.add("bg-dark");
+//         document.getElementById("header").style.backgroundImage = 'url(images/header-bg-dark-1.jpg)'
+
+//         document.getElementById("achievement").classList.add('bg-dark')
+//         document.getElementById("services").classList.add('bg-dark')
+// } else {
+//     document.getElementById("header").classList.remove("bg-dark");
+//     document.getElementById("header").style.backgroundImage = 'url(images/header-bg-light.jpg)'
+//     document.getElementById("achievement").classList.remove("bg-dark");
+//     document.getElementById("services").classList.remove("bg-dark");
+// }
